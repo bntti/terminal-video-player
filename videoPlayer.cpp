63ab6f8bbef3e37be8a028c6afd4b71afca553f7
Juho Röyskö <juho.roysko@paivola.fi>
2021-02-12 19:10:39 +0200

    Add arguments and option to disable statusbar. Add resolution to statusbar and reformat it

--- videoPlayer.cpp
+++ videoPlayer.cpp
@@ -12,16 +12,34 @@ int main(int argc, char **argv) {
 	std::ios_base::sync_with_stdio(false);
 	std::cin.tie(0);
 
+	bool statusBar = true;
+
+	// Manage arguments
+	if (argc < 2) {
+		std::cout << "Usage: " << argv[0] << " <args> <filename>" << '\n';
+		std::cout << "\t-d disable status bar.\n";
+		exit(0);
+	}
+	for (int i = 1; i < argc - 1; ++i) {
+		int len = strlen(argv[i]);
+		if (argv[i][0] != '-' || len < 2) {
+			std::cout << "Invalid argument: " << argv[i] << '\n';
+			exit(0);
+		}
+		for (int j = 1; j < len; ++j) {
+			if (argv[i][j] == 'd') statusBar = false;
+			else {
+				std::cout << "Invalid argument: -" << argv[i][j] << '\n';
+				exit(0);
+			}
+		}
+	}
+
 	// Hide cursor.
 	printf("\33[?25l");
 
-	if (argc < 2) {
-		std::cout << "Usage " << argv[0] << " <filename>" << std::endl;
-		return 0;
-	}
-
 	// Open video.
-	std::string filename = argv[1];
+	std::string filename = argv[argc-1];
 	cv::VideoCapture cap(filename);
 
 	// Check if video is open.
@@ -31,11 +49,11 @@ int main(int argc, char **argv) {
 	}
 
 	// Create variables.
-	std::vector<long double> frameTimes;
 	long double fps = cap.get(cv::CAP_PROP_FPS);
 	long double frameCount = 0;
 	auto start = std::chrono::steady_clock::now();
 	auto frameStart = std::chrono::steady_clock::now();
+	std::vector<long double> frameTimes;
 	cv::Mat frame;
 	while (1) {
 		// Read frame.
@@ -47,8 +65,8 @@ int main(int argc, char **argv) {
 		long double lines = w.ws_row;
 		long double cols = w.ws_col;
 
-		// Remove 2 from lines to prevent too many newlines.
-		lines -= 2;
+		// Remove 1 from lines to prevent too many newlines.
+		lines -= 1;
 
 		// Calculate font aspect ratio. (Assumes that terminal aspect ratio is 16:9)
 		long double fac = (cols * 9) / (lines * 16);
@@ -65,23 +83,28 @@ int main(int argc, char **argv) {
 		// Resize frame to previously calculated width and height.
 		cv::resize(frame, frame, cv::Size(std::floor(width), std::floor(height)));
 
-		// Calculate framerate
-		auto end = std::chrono::steady_clock::now();
-		long double milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(end - frameStart).count();
-		frameStart = std::chrono::steady_clock::now();
-		frameTimes.push_back(milliseconds);
-		int amount = 0;
-		long double sum = 0;
-		for (int i = frameCount; i > 0 && amount < 20; --i) {
-			++amount;
-			sum += frameTimes[i];
+		std::string status = "";
+		
+		if (statusBar) {
+			status += "Res: " + std::to_string((int)width) + "x" + std::to_string((int)(fac * height));
+
+			// Calculate framerate
+			auto end = std::chrono::steady_clock::now();
+			long double milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(end - frameStart).count();
+			frameStart = std::chrono::steady_clock::now();
+			frameTimes.push_back(milliseconds);
+			int amount = 0;
+			long double sum = 0;
+			for (int i = frameCount; i > 0 && amount < 20; --i) {
+				++amount;
+				sum += frameTimes[i];
+			}
+			status += "|fps: " + std::to_string((int)(1000 / (sum / amount)));
+
+			// Calculate how much the player is behind the original video.
+			milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+			status += "|behind by: " + std::to_string((int)std::max((long double)0, (1000 * (milliseconds / 1000 - frameCount / fps)))) + "ms";
 		}
-		std::string status = "Fps: " + std::to_string(1000 / (sum / amount));
-
-		// Calculate how much the player is behind the original video.
-		milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
-		status += " behind by: " + std::to_string((int)std::max((long double)0, (1000 * (milliseconds / 1000 - frameCount / fps)))) + "ms";
-
 		int i = 0;
 		int len = status.length();
 		std::string asciiFrame = "\33[0;0H";
@@ -107,8 +130,8 @@ int main(int argc, char **argv) {
 			asciiFrame += "\n";
 		}
 		// Check if the player should sleep.
-		end = std::chrono::steady_clock::now();
-		milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+		auto end = std::chrono::steady_clock::now();
+		long double milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
 		long double sleepDuration = frameCount / fps - milliseconds/1000;
 		if (sleepDuration > 0) std::this_thread::sleep_for(std::chrono::milliseconds((int)(sleepDuration*1000)));
 
